a911ca5970b9f27d14dddd4dd698f246c2f94d25
Guilherme Fontana <guifontana07@gmail.com>
2025-07-09 14:59:27 -0300

    feat: Coloca o repositorio JPA

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/MIssoes/MissoesService.java
@@ -0,0 +1,4 @@
+package dev.java10x.CadastroDeNinjas.MIssoes;
+
+public class MissoesService {
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/MIssoes/NinjaRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinjas.MIssoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository <MissoesModel, Long>{
+}
