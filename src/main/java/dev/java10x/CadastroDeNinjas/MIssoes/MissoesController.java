699a48cde80c35a0258e9d9b8940d29228334218
Guilherme Fontana <guifontana07@gmail.com>
2025-07-15 18:12:39 -0300

    fix: Inclui a anotation @JsonIgnore para prevenir o erro de serialização

--- src/main/java/dev/java10x/CadastroDeNinjas/MIssoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/MIssoes/MissoesController.java
@@ -1,6 +1,7 @@
 package dev.java10x.CadastroDeNinjas.MIssoes;
 
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,29 @@ public class MissoesController {
     public String bemVindoMissoes(){
         return "Bem vindo a pagina de missoes!";
     }
+
+
+
+    //missoes
+    @GetMapping("/missoes")
+    public String todasMissoes(){
+        return "Aqui voce podera ver as missoes e seus detalhes disponiveis!";
+    }
+
+    //status da missao
+    @GetMapping("/status")
+    public String statusMissoes(){
+        return "Aqui voce podera ver o status da sua missão atual";
+    }
+
+
+    //cancelar missao
+    @DeleteMapping("/cancelamento")
+    public String cancelarMissoes(){
+        return "Aqui voce podera cancelar sua missao e ser um otario! ";
+    }
+
+
+
 
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/MIssoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/MIssoes/MissoesModel.java
@@ -1,6 +1,7 @@
 package dev.java10x.CadastroDeNinjas.MIssoes;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -31,6 +32,7 @@ public class MissoesModel {
     //Uma missao pode ter varios ninjas
 
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List <NinjaModel> ninjas;
 
 
