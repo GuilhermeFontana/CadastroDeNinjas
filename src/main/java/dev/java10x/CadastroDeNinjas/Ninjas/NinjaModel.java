75206e1b136979f61ee91bc2cb02be72a06b774b
Guilherme Fontana <guifontana07@gmail.com>
2025-07-17 17:01:28 -0300

    fix: inclui a coluna de rank na minha entidade, porem ja estava incluida na migration v2

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -21,13 +21,7 @@ public class NinjaModel {
             @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     @Column(name = "nome_ninja")
     private String nome;
@@ -39,10 +33,60 @@ public class NinjaModel {
     @Column(name = "idade")
     private int idade;
 
+    @Column(name = "Rank")
+    private String rank;
+
     // o ninja tem somente uuma unica missao
     @ManyToOne
     @JoinColumn(name ="missoes_id") // foreing key
     private MissoesModel missoes;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
+    }
 }
