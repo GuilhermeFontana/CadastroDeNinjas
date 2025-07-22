4574bad316cd9f94a8ebc4a0c312f78e6ed8477a
Guilherme Fontana <guifontana07@gmail.com>
2025-07-22 12:00:29 -0300

    Chore: inclui a versao do DTO pela entidade e mapeia manualmente no missoes mapper

--- src/main/java/dev/java10x/CadastroDeNinjas/MIssoes/MissoesDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/MIssoes/MissoesDTO.java
@@ -1,38 +1,22 @@
 package dev.java10x.CadastroDeNinjas.MIssoes;
 
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.naming.NameAlreadyBoundException;
+
 import java.util.List;
 
-
-@Entity
-@Table(name = "tb_missoes")
 @NoArgsConstructor
 @AllArgsConstructor
-public class MissoesModel {
+public class MissoesDTO {
 
 
-    @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private long id;
-
     private String nome;
-
     private String dificuldade;
-
-    //Uma missao pode ter varios ninjas
-
-    @OneToMany(mappedBy = "missoes")
-    @JsonIgnore
-    private List <NinjaModel> ninjas;
+    private List<NinjaModel> ninjas;
 
     public long getId() {
         return id;
